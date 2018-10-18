037583f4d94df585253318f9dd3355d1cb9d42c3
Chris Norman <cnorman@broadinstitute.org>
2018-10-18 16:50:23 -0400

    URI - WIP.

--- src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/OptionalReadInputArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/OptionalReadInputArgumentCollection.java
@@ -2,12 +2,13 @@ package org.broadinstitute.hellbender.cmdline.argumentcollections;
 
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
-import org.broadinstitute.hellbender.utils.io.IOUtils;
+import org.broadinstitute.hellbender.engine.GatkInputURI;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * An argument collection for use with tools that accept zero or more input files containing reads
@@ -21,13 +22,13 @@ public final class OptionalReadInputArgumentCollection extends ReadInputArgument
             doc = "BAM/SAM/CRAM file containing reads",
             optional = true,
             common = true)
-    private List<String> readFilesNames = new ArrayList<>();
+    private List<GatkInputURI> readFilesNames = new ArrayList<>();
 
     @Override
     public List<File> getReadFiles() {
         ArrayList<File> ret = new ArrayList<>();
-        for (String fn : readFilesNames) {
-            ret.add(new File(fn));
+        for (GatkInputURI fn : readFilesNames) {
+            ret.add(fn.toPath().toFile());
         }
         return ret;
     }
@@ -35,14 +36,14 @@ public final class OptionalReadInputArgumentCollection extends ReadInputArgument
     @Override
     public List<Path> getReadPaths() {
         ArrayList<Path> ret = new ArrayList<>();
-        for (String fn : readFilesNames) {
-            ret.add(IOUtils.getPath(fn));
+        for (GatkInputURI fn : readFilesNames) {
+            ret.add(fn.toPath());
         }
         return ret;
     }
 
     @Override
     public List<String> getReadFilesNames() {
-        return new ArrayList<>(readFilesNames);
+        return new ArrayList<>(readFilesNames.stream().map(f -> f.getURIString()).collect(Collectors.toList()));
     }
 }
