20e5cbe2a42a1afc22477dfde2484058267bd71c
lichtens <lichtens@broadinstitute.org>
2018-10-18 12:59:37 -0400

    Maintenance commit.

--- src/main/java/org/broadinstitute/hellbender/tools/funcotator/FuncotatorConstants.java
+++ src/main/java/org/broadinstitute/hellbender/tools/funcotator/FuncotatorConstants.java
@@ -2,6 +2,11 @@ package org.broadinstitute.hellbender.tools.funcotator;
 
 public class FuncotatorConstants {
     /**
+     * Value to insert into fields when the annotation is unspecified, but still required.
+     */
+    public static final String UNKNOWN_VALUE_STRING = "__UNKNOWN__";
+
+    /**
      * Datasource name to use for Funcotations created from input variants from a VCF.
      */
     public static String DATASOURCE_NAME_FOR_INPUT_VCFS = "INPUT_VCF";
